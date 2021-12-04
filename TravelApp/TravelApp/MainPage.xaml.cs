1730f50e4eb21de9fffaeae87d9cf153ceeeca64
Habeeb Ajide <Ajidejibola@yahoo.com>
2021-12-04 09:40:55 +0100

    Add project files.

---
+++ TravelApp/TravelApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(emailEntry.Text) || string.IsNullOrEmpty(passwordEntry.Text)) 
+            {
+
+            }
+            Navigation.PushAsync(new HomePage());
+        }
+    }
+}
